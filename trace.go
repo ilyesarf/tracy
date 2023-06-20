b6325989be00c0be719970a7c5bb31c3c24f6d19
Quimzy <bam.boy2020@gmail.com>
2023-06-21 00:29:00 +0300

    parse log and retrieve useful info

--- trace.go
+++ trace.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 type SysCall struct {
-	Time string
-	OP   []string
+	Time string `json:"time"`
+	OP   string `json:"op"`
 }
 
 type Trace struct {
-	Binary   string
-	SysCalls []SysCall
+	Binary   string    `json:"binary"`
+	SysCalls []SysCall `json:"syscalls"`
 }
 
 func trace_bin(path string) {
@@ -38,12 +39,12 @@ func readLog() []string {
 	}
 	defer file.Close()
 
-	var syscalls []string
+	var unparsed_syscalls []string
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		syscalls = append(syscalls, line)
+		unparsed_syscalls = append(unparsed_syscalls, line)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -51,6 +52,21 @@ func readLog() []string {
 	}
 
 	os.Remove(logPath)
+	return unparsed_syscalls
+}
+
+func parseLog(unparsed_syscalls []string) []SysCall {
+	var syscalls []SysCall
+	for _, line := range unparsed_syscalls {
+		var syscall SysCall
+		if strings.Contains(line, "strace.c") || strings.Contains(line, "exit_group") {
+			splitted := strings.Split(line, " ")
+			syscall.Time = strings.Split(splitted[0], ":blink")[0]
+			syscall.OP = strings.Join(splitted[2:], " ")
+			syscalls = append(syscalls, syscall)
+		}
+	}
+
 	return syscalls
 }
 
@@ -66,8 +82,10 @@ func main() {
 	}
 
 	trace_bin(path)
+	unparsed_syscalls := readLog()
 
-	syscalls := readLog()
+	syscalls := parseLog(unparsed_syscalls)
+
 	for _, syscall := range syscalls {
 		fmt.Println(syscall)
 	}
